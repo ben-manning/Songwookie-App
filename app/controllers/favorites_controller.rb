a998380be24a703dec61174809874a9979a3a6d2
abhishekbhan <abbhan@gmail.com>
2015-01-27 10:58:06 -0800

    Start working on controllers

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,8 @@ class FavoritesController < ApplicationController
 
 	before_action :authenticate_user!
 
+	respond_to :html
+
 	def new
 		authorize@favorite
 	end
@@ -13,4 +15,16 @@ class FavoritesController < ApplicationController
 	def destroy
 		authorize@favorite
 	end
+
+
+
+	private
+    def set_favorite
+      @favorite_song = Song.find(params[:id])
+    end
+
+    def recipe_params
+      params.require(:song).permit(:song_name, :preview_url)
+    end
+
 end
