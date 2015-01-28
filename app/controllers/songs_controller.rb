4d6c9a75acd39b0966e7425b61d411cd4c72cce4
jcp510 <jcponce510@gmail.com>
2015-01-28 15:22:46 -0800

    Add vote methods to before_action in songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,7 @@
 class SongsController < ApplicationController
   
   before_action :authenticate_user!
+  before_action :set_song, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
 
   def index
   	@songs = RSpotify::Track.search(params[:song_name])
@@ -19,6 +20,6 @@ class SongsController < ApplicationController
     @song.downvote_from current_user
     redirect_to songs_path
   end
-  
+
 end
 
