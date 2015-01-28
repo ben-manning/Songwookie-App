dbd265bd8f1a46efeee3c2645b0b5ad0f03cfbe8
Ben Manning <bmanning@me.com>
2015-01-28 10:53:45 -0800

    add image tags

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,7 @@ class FavoritesController < ApplicationController
 	end
 
 	def index
+	    #RSpotify.authenticate("<2d492c018da541dfab1478cb2655dcaf>", "<2f77d68b97024008977eaa13ab6711a4>")
        @songs = RSpotify::Track.search(params[:song_name])
 		#@song = RSpotify::Track.find(params[:id])
 	end
